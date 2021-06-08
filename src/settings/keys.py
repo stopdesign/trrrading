5e49d8f18bfb4b8c5a51c55bae44215f561eca5a
Gregory Zhizhilkin <gregory@stopdesign.ru>
2021-06-08 10:34:59 +0200

    front, simple chart

---
+++ src/settings/keys.py
@@ -0,0 +1,28 @@
+# flake8: noqa
+# fmt: off
+
+live = [
+    ["8408eef4-3a43-438d-b4ad-56b9ec12b5d8", "13640cd0-e8a5-47f4-9e61-94d23d35112c", "wyayDzMl1red5o7bSxESD6LIrikjn70x"],  # noqa
+    ["0d1561d4-b5ce-464a-806b-93622facc503", "c8f02484-3797-4e2a-ae45-73be4867a3ec", "GX/4YbvFuP0HBUmrneUgVHAji2JHOQKA"],  # noqa
+    ["141e654d-7cb6-4817-9cc6-acd716c04e0c", "0bbc5fda-5ec7-467a-b4be-a39c7d7bd0b6", "zfmTsZl1IVOV3dcu3DSjem6mGmXCuTTP"],  # noqa
+    ["22688ad7-6fe1-40e1-b6a0-6f4a36bbc64f", "f2c5b586-d1c7-41cb-b692-54f27dad8ef2", "syjFeyVCgorVZRZizOLG+uGWdnuWCFNj"],  # noqa
+    ["3afa315f-649b-47ad-824d-6eb4274111a1", "6d1549f9-2f78-4041-a459-7c34957f58ee", "slhSvumieam12FKwlZKVuucM6wijchKo"],  # noqa
+    ["5e8a41f1-1628-4280-923d-590bc2a43345", "0cc6d8eb-ed3e-4c8b-9b3c-9b0d154b6984", "CBDQPXxap71ukumOwz1bxlnT25nOPDLx"],  # noqa
+    ["806dc10d-8d46-4c99-ba76-6f29c069d8f5", "28b03f6c-3cff-4b39-b2c4-400bd46f8798", "lE9VmYeG69mhPWqTHHw6JQKeYjSWoxbc"],  # noqa
+    ["92ee993e-6e18-49ec-b363-cce956c78f07", "b5c4ca9a-99be-42d5-8b6e-0f311b606c66", "5OLl4LMIM+L0slu1vA3FFa0oxMVU5Bdr"],  # noqa
+    ["9983883f-1b41-464c-8aa8-fc3bd9bbe381", "6b37b137-0b0e-4b84-a2e5-91c65d8e1c20", "KCV2kdVXXqMQgVFALe8rFT5HsumY4RER"],  # noqa
+    ["de30faf7-71cb-4f60-9b45-cec7f993ec5b", "557d9928-f9f9-42af-a390-10e5864c97d4", "/IJweFSaW4kxBHpqrgGeg6UcrBNTGczp"],  # noqa
+]
+
+demo = [
+    ["18ac3b58-d602-4b5a-a7c3-19259d12f51b", "6a3dd20c-0156-44e5-af49-33070159454e", "JPrfr4vxhQc7DkpEe4mG+HhzLlR1yIf1"],  # noqa
+    ["3f29df5b-a046-4f10-9a05-3ad58633b79e", "6841ae47-a7f0-4366-affe-a6df0e5c189f", "4fEOjBkYVqEotIsC/Zw1lQUPHJ/WbEhp"],  # noqa
+    ["4fef88f3-5f4e-4efb-8e06-b1288ba86a78", "3e188d59-d6af-4ce1-96b8-50afcba716cc", "881/sYb7Unnxogod345Q3Z4caVodiBku"],  # noqa
+    ["50bc5429-0900-4e63-98d5-2952d29ae59b", "21fc7139-b9f7-4470-a040-3bf009a4563e", "16C2s8jjtnJEpKfH2sTLi7MzSMaArqKm"],  # noqa
+    ["8bae08b2-5db3-4c76-a10c-4ef583fe4c6e", "6d4b874c-7ad8-41a9-a519-dbf80e7f47d8", "fqaQ35TT9HXy23skVuNoSg+ulE7RF1zv"],  # noqa
+    ["90fb5b9b-d701-4c08-9e1b-aebbe3d5f8f0", "c26618e5-d8ad-4b1e-a602-57fd2ff61e87", "/2gXlIv2sr/wgHVQGJUNMukbeTrHh1ry"],  # noqa
+    ["bf849eea-2d2b-4eb9-8ee3-33829a5ab389", "343115e2-002f-4b8b-9b49-f7246599c7e2", "jetk63nW4SRrF5dC+fkkBVo5N/eiD3VW"],  # noqa
+    ["e1b46116-6b4f-43dd-8d99-bd3ae0b8fb87", "96415535-387c-4a36-ba57-3256eabc87a4", "eyglTVr89TqZnckqvEWR3h+cOjK65Fvs"],  # noqa
+    ["e2d296a3-5eff-4a74-9ec9-8abeea361258", "aea1e829-cc96-4513-8712-cb7b42f7294a", "0rsJz+bxTnkaD+IQCIZ5Ox0gsKCseZv5"],  # noqa
+    ["e32aa839-ef99-4ecb-ba47-e09ac56e9968", "d5fd9d80-1b1b-4575-80a3-e869645068bd", "ZqcnWh7RXtkS0cG4yZCRXNST/xI9De4g"],  # noqa
+]
