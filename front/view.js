edb92ccdda51805a55cd0c1f4babd17f7163cc58
Gregory Zhizhilkin <gregory@stopdesign.ru>
2021-06-12 15:29:55 +0200

    front ES7

--- front/view.js
+++ front/view.js
@@ -1,13 +1,13 @@
-var margin = {top: 20, right: 50, bottom: 30, left: 50},
-  width = 2000 - margin.left - margin.right,
+const margin = {top: 20, right: 50, bottom: 30, left: 50},
+  width = 1500 - margin.left - margin.right,
   height = 750 - margin.top - margin.bottom;
 
-var parseDate = d3.timeParse('%Y-%m-%d %H:%M:%S');
-var dateFormat = d3.timeFormat('%Y-%m-%d %H:%M:%S');
-var valueFormat = d3.format(',.2f');
+const parseDate = d3.timeParse('%Y-%m-%d %H:%M:%S');
+const dateFormat = d3.timeFormat('%Y-%m-%d %H:%M:%S');
+const valueFormat = d3.format('+.2f');
 
-var dim = {
-  width: 2000, height: 1000,
+const dim = {
+  width: 1500, height: 1000,
   margin: {top: 20, right: 50, bottom: 30, left: 50},
   ohlc: {height: 750},
   indicator: {height: 100, padding: 0}
@@ -19,45 +19,50 @@ dim.plot = {
 dim.indicator.top = dim.ohlc.height + dim.indicator.padding;
 dim.indicator.bottom = dim.indicator.top + dim.indicator.height + dim.indicator.padding;
 
-var indicatorTop = d3.scaleLinear()
+const indicatorTop = d3.scaleLinear()
   .range([dim.indicator.top, dim.indicator.bottom]);
 
-var x = techan.scale.financetime().range([0, width]);
-var y = d3.scaleLinear().range([height, 0]);
+const x = techan.scale.financetime().range([0, width]);
+const y = d3.scaleLinear().range([height, 0]);
 
-var candlestick = techan.plot.candlestick()
+const candlestick = techan.plot.candlestick()
   .xScale(x)
   .yScale(y);
 
-var ohlc = techan.plot.ohlc()
+const ohlc = techan.plot.ohlc()
   .xScale(x)
   .yScale(y);
 
-var tradearrow = techan.plot.tradearrow()
+const tradearrow = techan.plot.tradearrow()
   .xScale(x)
   .yScale(y)
-  .orient(function (d) {
-    return d.type.startsWith("buy") ? "up" : "down";
-  })
+  .orient((d) => d.type.startsWith("buy") ? "up" : "down")
   .on("mouseenter", enter)
   .on("mouseout", out);
 
-var xAxis = d3.axisBottom(x);
-var xAxis1 = d3.axisBottom(x).ticks(30).tickFormat("");
+const xAxis = d3.axisBottom(x);
+const xAxis1 = d3.axisBottom(x);
 
-var yAxis = d3.axisLeft(y);
+const yAxis = d3.axisLeft(y);
 
-var yValueScale = d3.scaleLinear()
+const yValueScale = d3.scaleLinear()
   .range([indicatorTop(0) + dim.indicator.height, indicatorTop(0)]);
 
-var yDrawdownScale = d3.scaleLinear()
+const yDrawdownScale = d3.scaleLinear()
   .range([indicatorTop(0) + dim.indicator.height, indicatorTop(0) + dim.indicator.height * 2]);
 
-var yValueAxis = d3.axisRight(yValueScale).ticks(5);
-var yDrawdownAxis = d3.axisRight(yDrawdownScale).ticks(5).tickFormat((v, i) => { if (v > 0) return v });
+const yValueAxis = d3.axisRight(yValueScale)
+  .ticks(5)
+  .tickFormat((v, i) => v);
+
+const yDrawdownAxis = d3.axisRight(yDrawdownScale)
+  .ticks(5)
+  .tickFormat((v, i) => {
+    if (v > 0) return v + "%"
+  });
 
 // define the line
-var valueline1 = d3.area()
+const valueline1 = d3.area()
   .x(function (d) {
     return x(d.date);
   })
@@ -67,7 +72,7 @@ var valueline1 = d3.area()
   })
 
 // define the line
-var valueline2 = d3.area()
+const valueline2 = d3.area()
   .x(function (d) {
     return x(d.date);
   })
@@ -77,14 +82,14 @@ var valueline2 = d3.area()
   })
 
 
-var svg = d3.select("body").append("svg")
+const svg = d3.select("body").append("svg")
   .attr("width", dim.width)
   .attr("height", dim.height)
   .append("g")
   .attr("transform", "translate(" + dim.margin.left + "," + dim.margin.top + ")");
 // .call(zoom);
 
-var valueText = svg.append('text')
+const valueText = svg.append('text')
   .style("text-anchor", "end")
   .attr("class", "coords")
   .attr("x", width - 5)
@@ -102,7 +107,7 @@ svg.append("g")
   .attr("class", "candlestick")
   .attr("clip-path", "url(#clip)");
 
-var macd_svg = svg.append("g").attr("class", "macd");
+const macd_svg = svg.append("g").attr("class", "macd");
 
 svg.append("g")
   .attr("class", "x axis")
@@ -128,69 +133,59 @@ svg.append("g")
   .attr("class", "y-axis-dd")
   .attr("transform", "translate(" + (dim.width - dim.margin.left - dim.margin.right) + ",0)");
 
-d3.csv("data.csv", function (error, data) {
+svg.append("g")
+  .attr("class", "ohlc");
 
-  var accessor = candlestick.accessor();
+svg.append("g")
+  .attr("class", "tradearrow");
 
-  data = data.map(function (d) {
-    return {
-      date: parseDate(d.Date),
-      open: +d.Open,
-      high: +d.High,
-      low: +d.Low,
-      close: +d.Close,
-      volume: 10
-    };
-  }).sort(function (a, b) {
-    return d3.ascending(accessor.d(a), accessor.d(b));
-  });
+svg.append("g")
+  .attr("class", "y axis")
+  .append("text")
+  .attr("transform", "rotate(-90)")
+  .attr("y", 6)
+  .attr("dy", ".71em")
+  .style("text-anchor", "end")
+  .text("Price ($)");
 
-  svg.append("g")
-    .attr("class", "ohlc");
 
-  svg.append("g")
-    .attr("class", "tradearrow");
+async function run() {
+  let data = await d3.csv('data.csv');
+  let trades = await d3.csv('trades.csv');
+  let stats = await d3.csv('stats.csv');
 
-  svg.append("g")
-    .attr("class", "y axis")
-    .append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 6)
-    .attr("dy", ".71em")
-    .style("text-anchor", "end")
-    .text("Price ($)");
+  data = data.map((d) => ({
+    date: parseDate(d.Date),
+    open: +d.Open,
+    high: +d.High,
+    low: +d.Low,
+    close: +d.Close,
+    volume: 10
+  }));
 
-//        techanIntradayTimeInit = x.domain(data.map(accessor.d)).zoomable().copy();
+  trades = trades.map((d) => ({
+    date: parseDate(d.Date),
+    type: d.Direction,
+    price: d.Price
+  }));
 
-  var dataset = [];
-  d3.csv("trades.csv", function (trades_data) {
-    dataset = trades_data.map(function (d) {
-      return {
-        date: parseDate(d.Date),
-        type: d.Direction,
-        price: d.Price
-      };
-    });
+  stats = stats.map((d) => ({
+    date: parseDate(d.Date),
+    value: d.Value,
+    drawdown: d.Drawdown
+  }));
 
-    var stats_dataset = [];
-    d3.csv("stats.csv", function (stats_data) {
-      stats_dataset = stats_data.map(function (d) {
-        return {
-          date: parseDate(d.Date),
-          value: d.Value,
-          drawdown: d.Drawdown
-        };
-      });
-      draw(data, dataset, stats_dataset);
-    });
-  });
-
-});
-
+  draw(data, trades, stats);
+}
 
 function draw(data, trades, stats) {
-  var accessor = ohlc.accessor();
+  let accessor = ohlc.accessor();
 
+  data.sort((a, b) => (
+    d3.ascending(accessor.d(a), accessor.d(b))
+  ));
+
+  // techanIntradayTimeInit = x.domain(data.map(accessor.d)).zoomable().copy();
   x.domain(data.map(accessor.d));
   y.domain(techan.scale.plot.ohlc(data, accessor).domain());
 
@@ -203,14 +198,11 @@ function draw(data, trades, stats) {
     .call(tradearrow);
 
   yValueScale.domain([
-    d3.min(stats, function(d) { return +d.value; }),
-    d3.max(stats, function(d) { return +d.value; }),
+    d3.min(stats, (d) => +d.value),
+    d3.max(stats, (d) => +d.value),
   ]); //.nice();
 
-  yDrawdownScale.domain([
-    0,
-    d3.max(stats, function(d) { return +d.drawdown; }),
-  ]); //.nice();
+  yDrawdownScale.domain([0, d3.max(stats, (d) => +d.drawdown)]);
 
   macd_svg.append("path")
     .datum(stats)
@@ -224,16 +216,16 @@ function draw(data, trades, stats) {
 
   // add the Y gridlines
   svg.append("g")
-      .attr("class", "grid")
-      .call(
-          d3.axisLeft(yDrawdownScale).ticks(5).tickSize(-width).tickFormat("")
-      )
+    .attr("class", "grid")
+    .call(
+      d3.axisLeft(yDrawdownScale).ticks(5).tickSize(-width).tickFormat("")
+    )
   // add the Y gridlines
   svg.append("g")
-      .attr("class", "grid")
-      .call(
-          d3.axisLeft(yValueScale).ticks(5).tickSize(-width).tickFormat("")
-      )
+    .attr("class", "grid")
+    .call(
+      d3.axisLeft(yValueScale).ticks(5).tickSize(-width).tickFormat("")
+    )
 
   svg.selectAll("g.x.axis").call(xAxis);
   svg.selectAll("g.x-axis-2").call(xAxis1);
@@ -255,3 +247,6 @@ function out() {
 function refreshText(d) {
   valueText.text("Trade: " + dateFormat(d.date) + ", " + d.type + ", " + valueFormat(d.price));
 }
+
+run();
+
