7355882fddb71bd955ef8de52691b88793c72dd2
Gregory Zhizhilkin <gregory@stopdesign.ru>
2021-09-07 08:15:34 -0500

    ch br 4

--- src/strategy/__init__.py
+++ src/strategy/__init__.py
@@ -1,14 +1,17 @@
 from .signal import Signal
 from .base import BaseStrategy
 from .channel_breakout_3 import ChannelBreakout3
+from .channel_breakout_4 import ChannelBreakout4
 
 all_strategies = {
     "ChannelBreakout3": ChannelBreakout3,
+    "ChannelBreakout4": ChannelBreakout4,
 }
 
 __all__ = [
     "Signal",
     "BaseStrategy",
     "ChannelBreakout3",
+    "ChannelBreakout4",
     "all_strategies",
 ]
---
+++ src/strategy/channel_breakout_4.py
@@ -0,0 +1,106 @@
+import math
+import pandas as pd
+from talipp.indicators import DonchianChannels
+from talipp.ohlcv import OHLCV
+from strategy import BaseStrategy, Signal
+from data_types import Bar
+
+
+class ChannelBreakout4(BaseStrategy):
+    don = None
+    padding = 0
+    count_bars = False
+
+    def on_start(self):
+        self.padding = self.params.get("padding", 0)
+        self.count_bars = self.params.get("count_bars", False)
+        self.don = DonchianChannels(self.length)
+
+    def on_bar(self, pandas_ohlc):
+        bar = Bar.from_pandas(pandas_ohlc)
+
+        if bar.volume == 0:
+            return None
+
+        if self.data:
+            gap = abs(self.data[-1].close - bar.open)
+            prev_price = self.data[-1].close
+            prev_date = self.data[-1].date
+            if gap > 0.15:
+                price = prev_price
+                d_price = 0.005
+                cnt = math.ceil(gap / d_price)
+                cnt = min(cnt, 1500)
+                num = 0
+                for i in range(cnt):
+                    bar = Bar.from_pandas(pandas_ohlc)
+
+                    if bar.open > self.data[-1].close:
+                        price += d_price
+                    else:
+                        price -= d_price
+
+                    for ii in range(2):
+
+                        ohlcv = OHLCV(
+                            open=price,
+                            high=price,
+                            low=price,
+                            close=price,
+                        )
+
+                        self.don.add_input_value(ohlcv)
+
+                        if self.don:
+                            bar.up = self.don[-1].ub
+                            bar.dn = self.don[-1].lb
+                        else:
+                            bar.up = None
+                            bar.dn = None
+
+                        bar.open = price
+                        bar.high = price + 0.01
+                        bar.low = price - 0.01
+                        bar.close = price
+
+                        bar.date = prev_date + pd.Timedelta(seconds=num)
+                        num += 1
+
+                        self.data.append(bar)
+
+        if self.count_bars:
+            cnt = int(math.ceil(min(bar.barCount / self.count_bars, 20)))
+        else:
+            cnt = 1
+
+        for i in range(cnt):
+            self.don.add_input_value(pandas_ohlc)
+            bar = Bar.from_pandas(pandas_ohlc)
+
+            if self.don:
+                bar.up = self.don[-1].ub
+                bar.dn = self.don[-1].lb
+            else:
+                bar.up = None
+                bar.dn = None
+
+            self.data.append(bar)
+
+        return bar
+
+    def test_price(self, price: float) -> Signal:
+        """
+        Проверить сигнал стратегии при появлении новой цены.
+        """
+        bar = self.data[-1] if self.data else None
+
+        if not bar or not bar.dn:
+            return Signal.PASS
+
+        if price > bar.up - self.padding:
+            return Signal.LONG
+
+        if price < bar.dn + self.padding:
+            return Signal.SHORT
+
+        return Signal.PASS
