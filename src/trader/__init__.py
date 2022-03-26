9b661c10647604246c35523ba35d1c09e8bebd8d
Gregory Zhizhilkin <gregory@stopdesign.ru>
2022-03-26 16:01:39 -0700

    fix deploy issues

--- src/trader/__init__.py
+++ src/trader/__init__.py
@@ -2,14 +2,12 @@ from .advisor import Advisor
 from .exchange import Exchange
 from .portfolio import Portfolio
 from .executor import Executor
-from .tg_bot import TelegramBotMixin
 from .trader import Trader
 
 __all__ = [
     "Advisor",
     "Trader",
     "Portfolio",
-    "TelegramBotMixin",
     "Executor",
     "Exchange",
 ]
