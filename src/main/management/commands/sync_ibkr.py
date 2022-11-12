f353fa4d273e5581bc7bdb2fd67eed2c4632c44c
Gregory <gregory@stopdesign.ru>
2022-11-12 09:44:23 -0800

    sync support for ZB price

--- src/main/management/commands/sync_ibkr.py
+++ src/main/management/commands/sync_ibkr.py
@@ -54,13 +54,18 @@ def fractions_to_float(frac_str):
     try:
         return float(frac_str)
     except ValueError:
-        num, denom = frac_str.split('/')
-        try:
-            leading, num = num.split(' ')
-            whole = float(leading)
-        except ValueError:
-            whole = 0
-        frac = float(num) / float(denom)
+        if "'" in frac_str:
+            whole, frac = frac_str.split("'")
+            whole = float(whole)
+            frac = float(frac) / 320 * 100
+        else:
+            num, denom = frac_str.split('/')
+            try:
+                leading, num = num.split(' ')
+                whole = float(leading)
+            except ValueError:
+                whole = 0
+            frac = float(num) / float(denom)
         return whole - frac if whole < 0 else whole + frac
 
 
