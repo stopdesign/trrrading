4679d3aca568f2eab4ab6acf9aa254034525cd10
Gregory <gregory@stopdesign.ru>
2023-05-03 20:10:23 -0700

    ghs_live

--- src/trader/strategy/GHS_live.py
+++ src/trader/strategy/GHS_live.py
@@ -8,16 +8,17 @@ from trader.strategy import BaseStrategy
 ALGO = {"strategy": "Adaptive"}
 
 
-class GHS_01(BaseStrategy):
+class GHS_live(BaseStrategy):
     """
     Стратегия для зерновых фьючерсов.
     """
 
     length: int = 150
-    length_ma_1: int = 50
-    length_ma_2: int = 100
+    length_ma_1: int = 0
+    length_ma_2: int = 0
     timeframe: str = "10m"
     rth_data: bool = True
+    amount: int = 1
 
     def on_start(self):
         # Подписка на данные
@@ -31,14 +32,16 @@ class GHS_01(BaseStrategy):
         )
 
         # Добавление индикатораов
-        self.ma_1 = MovingAverage(self.data_1m, length=self.length_ma_1)
-        self.ma_2 = MovingAverage(self.data_1m, length=self.length_ma_2)
         self.hma = HullMA(
             self.data_tf,
             length=self.length,
             skip_extra_hours=self.rth_data,
             skip_empty=True,
         )
+        if self.length_ma_1:
+            self.ma_1 = MovingAverage(self.data_1m, length=self.length_ma_1)
+        if self.length_ma_2:
+            self.ma_2 = MovingAverage(self.data_1m, length=self.length_ma_2)
 
     def liquid_hours(self, bar):
         """
@@ -50,8 +53,10 @@ class GHS_01(BaseStrategy):
 
     def get_amount(self, price) -> int:
         # Подсчет размера позиции
-        # return 1
-        return int(100_000 / price)
+        if self.amount > 0:
+            return self.amount
+        else:
+            return int(100_000 / price)
 
     def on_tick(self, trade: Trade):
         """
@@ -71,15 +76,17 @@ class GHS_01(BaseStrategy):
         current_amount = int(self.positions[self.sid].amount)
         target_amount = current_amount
 
-        diff = abs(hma - prev_hma) / hma * 100
-        ma_diff = abs(self.ma_1.value["ma"] - self.ma_2.value["ma"])
+        if self.length_ma_1 and self.length_ma_2:
+            diff = abs(hma - prev_hma) / hma * 100
+            ma_diff = abs(self.ma_1.value["ma"] - self.ma_2.value["ma"])
+            do_trade = ma_diff > 0.5 and diff > 0.001
+        else:
+            do_trade = True
 
-        filter = ma_diff > 0.5 and diff > 0.001
-
-        if current_amount <= 0 and hma > prev_hma and filter:
+        if current_amount <= 0 and hma > prev_hma and do_trade:
             target_amount = +self.get_amount(trade.price)
 
-        if current_amount >= 0 and hma < prev_hma and filter:
+        if current_amount >= 0 and hma < prev_hma and do_trade:
             target_amount = -self.get_amount(trade.price)
 
         if target_amount != current_amount:
